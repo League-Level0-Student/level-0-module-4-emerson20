149da393f5972480695c71983531d15dba2a4be1
League <league@WTS9.attlocal.net>
2018-06-10 17:33:47 -0700

    HappyPet

--- src/HappyPet.java
+++ src/HappyPet.java
@@ -4,26 +4,47 @@ public class HappyPet {
 	
 	// 2. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	
+	static int happinessLevel = 0;
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
+		String pet = JOptionPane.showInputDialog("What is your dream kind of pet to buy at the store?");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
 		
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
-
+			int task = JOptionPane.showOptionDialog(null, "What kind thing do you want to do to make your dream pet happy?", "Your Kindness", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "read to it ", "feed it ", "make sure itis always healthy " }, null);
+System.out.println(task);
 			// 5. Use user input to call the appropriate method created in step 4.
-
+if (task == 0 ) {
+	happyPetAtHome(pet);
+}else if (task == 1) {
+	theAnimalToBeFead(pet);
+}else if (task == 2) {
+	theHealthyWay(pet);
+}
 			// 6. If you determine the happiness level is large enough, tell the
-			//    user that he loves his pet and use break; to exit for loop.
+			//    user that he loves his pet and use break; to exit for 
+if (happinessLevel == 420) {
+	JOptionPane.showMessageDialog(null, "Wow! You really love your pet.");
+}
 
 	}
 
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+	static void happyPetAtHome(String pet) {
+		JOptionPane.showMessageDialog(null, "A "+pet+" might fall asleep or relaxe when read to.");
+		happinessLevel = happinessLevel +5; 
+	}
+	static void theAnimalToBeFead(String pet) {
+		JOptionPane.showMessageDialog(null, "A "+pet+" might love you even more when you feed it.");
+		happinessLevel = happinessLevel +10; 
+	}
+	static void theHealthyWay(String pet) {
+		JOptionPane.showMessageDialog(null,"A "+pet+" might be thrilled to see you 'cause it knows it is safe with you.");
+		happinessLevel = happinessLevel +15; 	
+	}
 }
