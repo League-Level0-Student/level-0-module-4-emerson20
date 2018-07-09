ffb551413b8e8a880597cb4da95a6b9cad9a866f
emerson20 <league@WTS-IM-17.attlocal.net>
2018-07-08 17:34:15 -0700

    String Methods 1

--- src/StringMethods1.java
+++ src/StringMethods1.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
@@ -5,18 +7,21 @@
 
 public class StringMethods1 {
 	public static void main(String[] args) {
-		
+		 
+	
 		// 1. Create a String variable and initialize it to whatever you want
-
+	String cat = "mermaids";
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-		
+System.out.println(cat.charAt(2));
 		// 3. Print the length of your String to the console.
 		//    HINT: .length
-
+System.out.println(cat.length());
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
+for (int i = 8; i < cat.length(); i++) {
+	
+}
 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
